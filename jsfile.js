901625ab9f8e2ae981374a3a8b95d78b65b7f100
GeorgesxD <148150023+GeorgesxD@users.noreply.github.com>
2024-04-21 19:09:07 +0300

    Update jsfile.js

--- jsfile.js
+++ jsfile.js
@@ -1,40 +1,35 @@
 function fp_alert(){
-    alert("We sent you an email to change your password")
+  alert("We sent you an email to change your password")
 }
 
-
-const users = ["Georges","Hassan"];
-const pws = ["Georges123","Hassan123"];
-const emails = ["gkl001lb@aou.edu.lb","hta008lb@aou.edu.lb"];
-
+const userinfo = [{"username":"Georges","email":"gkl001lb@aou.edu.lb","password":"Georges123"},{"username":"Hassan","email":"hta008lb@aou.edu.lb","password":"Hassan123"}]
 
 function login(){
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
-  for(let i=0;i<users.length;i++){
-    if ((username === users[i] && password === pws[i]) || (username === emails[i] && password === pws[i]) ) {
+  for(let i=0;i<userinfo.length;i++){
+    if ((username === userinfo[i].username && password === userinfo[i].password) || (username === userinfo[i].email && password === userinfo[i].password) ) {
         alert("Login successful!");
         break;
         
-    } else if(i===users.length-1) {
+    } else if(i===userinfo.length-1) {
         alert("Invalid Credentials, Please try again.");
     }
   }
 };
-
 
 function signup(){
   let username2 = document.getElementById("username").value;
   let email2 = document.getElementById("email").value;
   let password2 = document.getElementById("password").value;
   if(username2 !="" && email2 !="" && password2 !=""){
-    users.push(username2);
-    emails.push(email2);
-    pws.push(password2);
-    alert("User Successfully Created.")
+    new_user={"username":username2,"email":email2,"password":password2};
+    userinfo.push(new_user);
+    console.log(userinfo);
+    alert("User Successfully Created.");
     
   }
   else{
-    alert("Missing Information. Please fill out all of the boxes.")
+    alert("Missing Information. Please fill out all of the boxes.");
   }
 };
